37334c79cdd37b5b4195ad83bbdf1ba668bc7979
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-26 15:37:22 +0700

    testing

--- 03 WebSrapping/quotes/quotes/spiders/spidercompletequote.py
+++ 03 WebSrapping/quotes/quotes/spiders/spidercompletequote.py
@@ -11,16 +11,37 @@ class SpidercompletequoteSpider(scrapy.Spider):
         quotes = response.css(".quote")
         
         for quote in quotes:
-            tags = quote.css(".tags")
+            # tags = quote.css(".tags")
             
-            yield {
-                'quotes' : quote.css(".text::text").get(),
-                'author' : quote.css(".author::text").get(),
-                'tag' : tags.css(".tag::text").extract()
-            }
+            # yield {
+            #     'quotes' : quote.css(".text::text").get(),
+            #     'author' : quote.css(".author::text").get(),
+            #     'tag' : tags.css(".tag::text").extract()
+            # }
+
+            relative_url = quote.xpath("//small[@class = 'author']//following-sibling::a/@href").getall()
+
+            quote_url = 'https://quotes.toscrape.com' +  relative_url
+
+            yield response.follow(quote_url, callback = self.parse_page)
+
         
-        nextpage = response.css(".pager .next a::attr(href)").get()
+        # nextpage = response.css(".pager .next a::attr(href)").get()
 
-        nexpageurl = 'https://quotes.toscrape.com/' +  nextpage
+        # nexpageurl = 'https://quotes.toscrape.com/' +  nextpage
 
-        yield response.follow(nexpageurl, callback = self.parse)
+        # yield response.follow(nexpageurl, callback = self.parse)
+
+
+
+    def parse_page(self, response):
+
+
+        yield {
+            "author_name" : response.css(".author-title::text").get(),
+            "author_born_date" : response.css(".author-born-date::text").get(),
+            "author_born_place" : response.css(".author-born-location::text").get(),
+            "author_description" : response.css(".author-description::text").get()
+        }
+
+        
